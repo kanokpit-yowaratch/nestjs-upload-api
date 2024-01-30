214f0acf31e9e28786e26e76c2c507dd4e2207d0
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-01-30 21:52:06 +0700

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Upload example')
+    .setDescription('The Upload API description')
+    .setVersion('1.0')
+    .addTag('Upload')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(process.env.API_PORT);
 }
 bootstrap();
