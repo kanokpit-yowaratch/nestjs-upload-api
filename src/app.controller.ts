ec12a75a1b7903794d0f7a329e1a818fb2d95513
Kanokpit Yowaratch <kanokpit_yow_fth@tdem.toyota-asia.com>
2024-09-25 23:11:41 +0700

    feat: prepare to more feature

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,28 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, FileTypeValidator, Get, HttpException, HttpStatus, MaxFileSizeValidator, Param, ParseFilePipe, Post, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 import { ApiTags, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { UploadDto } from './dto/upload.dto';
 import { Request as URLRequest } from "express";
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+export const editFileName = (req, file, callback) => {
+  const name = file.originalname.split('.')[0];
+  const fileExtName = extname(file.originalname);
+  const randomName = Array(4)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 16).toString(16))
+    .join('');
+  callback(null, `${name}-${randomName}${fileExtName}`);
+};
+
+export const imageFileFilter = (req, file, callback) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new Error('Only image files are allowed!'), false);
+  }
+  callback(null, true);
+};
 
 @ApiTags('Upload')
 @Controller()
@@ -25,11 +44,41 @@ export class AppController {
     type: UploadDto,
     description: 'Json structure for upload object',
   })
-  @UseInterceptors(FileInterceptor('file'))
+  // @UseInterceptors(FileInterceptor('file'))
+   // Try this
+  @UseInterceptors(FileInterceptor('image', {
+    storage: diskStorage({
+      destination: './files',
+      filename: editFileName,
+    }),
+    fileFilter: imageFileFilter,
+  }))
+  // Alternative
+  // @UseInterceptors(FileInterceptor('file', {
+  //   storage: diskStorage({
+  //     destination: './uploads'
+  //     , filename: (req, file, cb) => {
+  //       // Generating a 32 random chars long string
+  //       const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+  //       //Calling the callback passing the random name generated with the original extension name
+  //       cb(null, `${randomName}${extname(file.originalname)}`)
+  //     }
+  //   })
+  // }))
   async uploadFile(
     // @Body() uploadDto: UploadDto, => Required all column exist in table => Not recommened, it has effect to table structure
     @Body() formData: Request, // Not need all column
-    @UploadedFile() file: Express.Multer.File
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2000 }),
+          // new FileTypeValidator({ fileType: 'image/jpeg' }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' })
+        ],
+        fileIsRequired: false,
+      }),
+    )
+    file: Express.Multer.File
   ) {
     const fileName = file?.filename;
     if (!fileName) {
@@ -40,6 +89,7 @@ export class AppController {
     const isDup = await this.appService.isDuplicateCode(imageCode);
 
     if (isDup) {
+      // remove file upload previously
       throw new HttpException('Duplicate image code.', HttpStatus.BAD_REQUEST);
     }
 
